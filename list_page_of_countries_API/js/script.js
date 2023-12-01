cd206b26a278f0ca0ca503673ec28c41c9c0ca2e
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-12-01 18:51:47 +0200

    Update script.js

--- list_page_of_countries_API/js/script.js
+++ list_page_of_countries_API/js/script.js
@@ -1,35 +1,36 @@
-const body = document.querySelector('body');
-const siteThemeSwitcher = document.querySelector(".switch-slider");
+const $ = document.querySelector.bind(document);
 
-const dropDownBtn = document.querySelector(".dropdown-button");
-const dropDownList = document.querySelector(".dropdown-list");
-const dropDownListItems = dropDownList.querySelectorAll(".dropdown-item");
-const dropDownInput = document.querySelector(".dropdown-input-hidden");
+const siteThemeSwitcher = $(".switch-slider");
 
-const inputSearch = document.querySelector(".search");
-const searchContry = document.querySelector("#countrySearch");
+const dropDownBtn = $(".dropdown-button");
+const dropDownList = $(".dropdown-list");
+const dropDownListItems = dropDownList.querySelectorAll("li");
+const dropDownInput = $(".dropdown-input-hidden");
 
-const allCards = document.querySelector(".contries-cards-container");
+const inputSearch = $(".search");
+const searchCountry = $("#countrySearch");
+
+const allCards = $(".countries-cards-container");
 
 // Light/Dark Mood
 siteThemeSwitcher.addEventListener("click", function () {
-    body.classList.toggle("dark-mood");
+    document.body.classList.toggle("dark-mood");
 });
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    fetchData();
+    getCountries();
 });
 
 // REST Countries API 
-async function fetchData() {
+async function getCountries() {
     try {
         const url = `https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags`;
         const response = await fetch(url);
         const data = await response.json();
-        getContries(data);
-        getUserSearchingContry(data);
+        renderCards(data);
+        getCountrySearch(data);
         filterRegion(data);
-        getSelectedItemKeyDownUp(data);
+        getSelectedItem(data);
         // console.log(data);
     } catch (error) {
         console.log(error); 
@@ -37,46 +38,49 @@ async function fetchData() {
 }
 
 // Render card with contries HTML
-function getContries(data) {
+function renderCards(countryInfo) {
     let contriesCardHTML = '';
-    data.forEach((item) => {
+    allCards.innerHTML = countryInfo.map((item) => {
         contriesCardHTML += `
-            <div class="contries-item">
+            <div class="countries-item">
                 <img src="${item.flags.png}" alt="Country flag">
-                <div class="contrie-info">
-                    <div class="contrie-name">${item.name.official}</div>
-                    <div class="contrie-population"><span>Population:</span> ${item.population.toLocaleString()}</div>
-                    <div class="contrie-region"><span>Region:</span> ${item.region}</div>
-                    <div class="contrie-capital"><span>Capital:</span> ${item.capital}</div>
+                <div class="countrie-info">
+                    <h2>${item.name.official}</h2>
+                    <div><span>Population:</span> ${item.population.toLocaleString()}</div>
+                    <div><span>Region:</span> ${item.region}</div>
+                    <div><span>Capital:</span> ${item.capital}</div>
                 </div>
             </div>
         `;
-    });
+    }).join("");
+
     allCards.innerHTML = contriesCardHTML;
 }
 
 // Search country - input
-function getUserSearchingContry(data) {
-    inputSearch.addEventListener('keyup', event => {
-        event.preventDefault();
-        const customerLetters = searchContry.value.toLowerCase();
-        console.log(customerLetters);
-        const filteringArrayCountries = data.filter((item) => {
-            const lettersApi = item.name.official.toLowerCase();
-            if (lettersApi.indexOf(customerLetters) !== -1) {
-                return item;
-            }
-        })
-        getContries(filteringArrayCountries);
-    })
+function getCountrySearch(nameCountries) {
+    inputSearch.addEventListener('keypress', event => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            const userInput = searchCountry.value.toLowerCase();
+            console.log(userInput);
+            const filteredArrayCountries = nameCountries.filter((nameCountry) => {
+                const letters = nameCountry.name.official.toLowerCase();
+                if (letters.indexOf(userInput) !== -1) {
+                    return nameCountry;
+                }
+            });
+            renderCards(filteredArrayCountries);
+            searchCountry.value = '';
+            dropDownBtn.innerText = 'Region';
+        }
+    });
 }
 
 // Select - Region
 // Click on the button, open/close select
 dropDownBtn.addEventListener("click", function () {
-    dropDownList.classList.toggle("dropdown-list--visible");
-    this.classList.add("dropdown-button--active");
-    this.classList.toggle("dropdown-button--clicked");
+    this.classList.toggle("dropdown-button--active");
 });
 
 // Selecting an element from the list. Remember the selected value + filter button "Region". Close dropdown
@@ -88,14 +92,13 @@ function filterRegion(data) {
             dropDownBtn.innerText = this.innerText;
             dropDownBtn.focus();
             dropDownInput.value = this.dataset.value;
-            dropDownList.classList.remove("dropdown-list--visible");
-            dropDownBtn.classList.remove("dropdown-button--clicked");
+            dropDownBtn.classList.remove("dropdown-button--active");
 
             if (this.dataset.value === "" || this.dataset.value === "All") {
-                getContries(data);
+                renderCards(data);
             } else {
                 const arrayFilter = data.filter((item) => item.region === this.dataset.value);
-                getContries(arrayFilter);
+                renderCards(arrayFilter);
             }
         });
     });
@@ -107,66 +110,62 @@ for (let i = 0; i < dropDownListItems.length; i++) {
         const activeClass = document.querySelectorAll('.active');
 
         if (activeClass.length > 0) {
-            activeClass[0].className = activeClass[0].className.replace(' active', '');
+            activeClass[0].className = activeClass[0].className.replace('active', '');
         }
-        this.className += " active";
+        this.className += "active";
     });
 }
 
 // Click outside dropdown - close dropdown
 document.addEventListener("click", function (event) {
     if (event.target !== dropDownBtn) {
-        dropDownList.classList.remove("dropdown-list--visible");
         dropDownBtn.classList.remove("dropdown-button--active");
-        dropDownBtn.classList.remove("dropdown-button--clicked");
     }
 });
 
 // Pressing Tab or Escape - close dropdown
 document.addEventListener("keydown", function (event) {
     if (event.key === "Tab" || event.key === "Escape") {
-        dropDownList.classList.remove("dropdown-list--visible");
         dropDownBtn.classList.remove("dropdown-button--active");
-        dropDownBtn.classList.remove("dropdown-button--clicked");
     }
 });
 
 // Pressing Enter when the list of regions is open - select a region, arrows navigate through the list
-function getSelectedItemKeyDownUp(data) {
+function getSelectedItem(nameRegion) {
     document.addEventListener("keydown", function (event) {
-        const dropDownListItemSelected = dropDownList.querySelector('.dropdown-item.active');
+        const dropDownListItemSelected = dropDownList.querySelector('li.active');
 
-        if (dropDownList.classList.contains("dropdown-list--visible") && dropDownBtn.classList.contains('dropdown-button--active')) {
-            if (event.key === "Enter") {
-                dropDownBtn.innerText = dropDownListItemSelected.innerHTML;
-
-                if (dropDownListItemSelected.innerHTML === "" || dropDownListItemSelected.innerHTML === "All") {
-                    getContries(data);
-                } else {
-                    const arrayFilterClickEnter = data.filter((item) => item.region === dropDownListItemSelected.innerHTML);
-                    getContries(arrayFilterClickEnter);
-                }
-                console.log(dropDownListItemSelected.innerHTML);
-            } else if (event.key === "ArrowDown") {
-                event.preventDefault();
-                for (let i = 0; i < dropDownListItems.length; i++) {
-                    if (dropDownListItems[i].classList.contains('active') && i !== dropDownListItems.length - 1) {
-                        dropDownListItems[i + 1].classList.add("active");
-                        dropDownListItems[i].classList.remove("active");
-
-                        break;
+        if (dropDownList.classList.contains("dropdown-list") && dropDownBtn.classList.contains('dropdown-button--active')) {
+            switch (event.key) {
+                case "Enter":
+                    dropDownBtn.innerText = dropDownListItemSelected.innerHTML;
+                    if (dropDownListItemSelected.innerHTML === "" || dropDownListItemSelected.innerHTML === "All") {
+                        renderCards(nameRegion);
+                    } else {
+                        const arrayFilterClickEnter = nameRegion.filter((item) => item.region === dropDownListItemSelected.innerHTML);
+                        renderCards(arrayFilterClickEnter);
                     }
-                }
-            } else if (event.key === "ArrowUp") {
-                event.preventDefault();
-                for (let i = 0; i < dropDownListItems.length; i++) {
-                    if (dropDownListItems[i].classList.contains('active') && i !== 0) {
-                        dropDownListItems[i - 1].classList.add('active');
-                        dropDownListItems[i].classList.remove('active');
-
-                        break;
+                    break;
+                case "ArrowDown":
+                    event.preventDefault();
+                    for (let i = 0; i < dropDownListItems.length; i++) {
+                        if (dropDownListItems[i].classList.contains('active') && i !== dropDownListItems.length - 1) {
+                            dropDownListItems[i + 1].classList.add("active");
+                            dropDownListItems[i].classList.remove("active");
+                            break;
+                        }
                     }
-                }
+                    break;
+                case "ArrowUp":
+                    event.preventDefault();
+                    for (let i = 0; i < dropDownListItems.length; i++) {
+                        if (dropDownListItems[i].classList.contains('active') && i !== 0) {
+                            dropDownListItems[i - 1].classList.add('active');
+                            dropDownListItems[i].classList.remove('active');
+                            break;
+                        }
+                    }
+                    break;
             }
         }
     });
